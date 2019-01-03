87b2d9ef9edd33fb6e961f6083f82dd15a23d80f
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-03 12:35:05 -0300

    Initial Project Setup

--- app/src/main/java/com/fffrowies/fragmenttest2/FragmentA.java
+++ app/src/main/java/com/fffrowies/fragmenttest2/FragmentA.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,76 +18,31 @@ public class FragmentA extends Fragment {
 
     private static final String TAG = FragmentA.class.getSimpleName();
 
+    private Button btnAdd;
+    private TextView txvResult;
+
     public FragmentA() {
         // Required empty public constructor
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        Log.i(TAG, "onAttach()");
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate()");
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        Log.i(TAG, "onCreateView()");
-        return inflater.inflate(R.layout.fragment_a, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_a, container, false);
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Log.i(TAG, "onActivityCreated()");
-    }
+        txvResult = view.findViewById(R.id.txvResult);
+        btnAdd = view.findViewById(R.id.btnAdd);
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart()");
-    }
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code to add two numbers
+            }
+        });
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume()");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.i(TAG, "onPause()");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop()");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Log.i(TAG, "onDestroyView()");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy()");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        Log.i(TAG, "onDetach()");
+        return view;
     }
 }
 
