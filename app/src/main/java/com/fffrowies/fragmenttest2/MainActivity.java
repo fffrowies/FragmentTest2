7d355cb1d29a2b8b12f8fd0f07c2893d66178817
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-03 09:46:21 -0300

    Performing Add and Remove Transactions

--- app/src/main/java/com/fffrowies/fragmenttest2/MainActivity.java
+++ app/src/main/java/com/fffrowies/fragmenttest2/MainActivity.java
@@ -1,28 +1,65 @@
 package com.fffrowies.fragmenttest2;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    FragmentManager manager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manager = getSupportFragmentManager();
     }
 
     public void addFragmentA(View view) {
+
+        FragmentA fragmentA = new FragmentA();
+
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container, fragmentA, "fragA");
+        transaction.commit();
     }
 
     public void removeFragmentA(View view) {
+
+        FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
+        FragmentTransaction transaction = manager.beginTransaction();
+        if (fragmentA != null) {
+            transaction.remove(fragmentA);
+            transaction.commit();
+        } else {
+            Toast.makeText(this, "Fragment A not Found", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void addFragmentB(View view) {
+
+        FragmentB fragmentB = new FragmentB();
+
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container, fragmentB, "fragB");
+        transaction.commit();
     }
 
     public void removeFragmentB(View view) {
+
+        FragmentB fragmentB = (FragmentB) manager.findFragmentByTag("fragB");
+        FragmentTransaction transaction = manager.beginTransaction();
+        if (fragmentB != null) {
+            transaction.remove(fragmentB);
+            transaction.commit();
+        } else {
+            Toast.makeText(this, "Fragment B not Found", Toast.LENGTH_SHORT).show();
+        }
     }
 }
