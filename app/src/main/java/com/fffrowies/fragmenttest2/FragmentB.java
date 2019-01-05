f220ac8b6a0df596f04c4c56183d98dc3226c004
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-05 08:16:38 -0300

    Initial Project Setup

--- app/src/main/java/com/fffrowies/fragmenttest2/FragmentB.java
+++ app/src/main/java/com/fffrowies/fragmenttest2/FragmentB.java
@@ -21,70 +21,10 @@ public class FragmentB extends Fragment {
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        Log.e(TAG, "onAttach()");
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.e(TAG, "onCreate()");
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         Log.e(TAG, "onCreateView()");
         return inflater.inflate(R.layout.fragment_b, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Log.e(TAG, "onActivityCreated()");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.e(TAG, "onStart()");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.e(TAG, "onResume()");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.e(TAG, "onPause()");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.e(TAG, "onStop()");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Log.e(TAG, "onDestroyView()");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.e(TAG, "onDestroy()");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        Log.e(TAG, "onDetach()");
     }
 }
--- app/src/main/java/com/fffrowies/fragmenttest2/MainActivity.java
+++ app/src/main/java/com/fffrowies/fragmenttest2/MainActivity.java
@@ -4,10 +4,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentManager.OnBackStackChangedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -19,23 +20,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         manager = getSupportFragmentManager();
+        manager.addOnBackStackChangedListener(this);
     }
 
     public void addFragmentA(View view) {
-
         FragmentA fragmentA = new FragmentA();
 
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.container, fragmentA, "fragA");
+        transaction.addToBackStack("AddFragA");
         transaction.commit();
     }
 
     public void removeFragmentA(View view) {
-
         FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
         FragmentTransaction transaction = manager.beginTransaction();
         if (fragmentA != null) {
             transaction.remove(fragmentA);
+            transaction.addToBackStack("RemFragA");
             transaction.commit();
         } else {
             Toast.makeText(this, "Fragment A not Found", Toast.LENGTH_SHORT).show();
@@ -43,20 +45,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addFragmentB(View view) {
-
         FragmentB fragmentB = new FragmentB();
 
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.container, fragmentB, "fragB");
+        transaction.addToBackStack("AddFragB");
         transaction.commit();
     }
 
     public void removeFragmentB(View view) {
-
         FragmentB fragmentB = (FragmentB) manager.findFragmentByTag("fragB");
         FragmentTransaction transaction = manager.beginTransaction();
         if (fragmentB != null) {
             transaction.remove(fragmentB);
+            transaction.addToBackStack("RemFragB");
             transaction.commit();
         } else {
             Toast.makeText(this, "Fragment B not Found", Toast.LENGTH_SHORT).show();
@@ -64,28 +66,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void replaceByFragmentA(View view) {
-
         FragmentA fragmentA = new FragmentA();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(R.id.container, fragmentA, "fragA");
+        transaction.addToBackStack("RepByFragA");
         transaction.commit();
     }
 
     public void replaceByFragmentB(View view) {
-
         FragmentB fragmentB = new FragmentB();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(R.id.container, fragmentB, "fragB");
+        transaction.addToBackStack("RepByFragB");
         transaction.commit();
     }
 
     public void attachFragmentA(View view) {
-
         FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
         FragmentTransaction transaction = manager.beginTransaction();
 
         if (fragmentA != null) {
             transaction.attach(fragmentA);
+            transaction.addToBackStack("AttachFragA");
             transaction.commit();
         } else {
             Toast.makeText(this, "Fragment A not Found", Toast.LENGTH_SHORT).show();
@@ -93,12 +95,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void detachFragmentA(View view) {
-
         FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
         FragmentTransaction transaction = manager.beginTransaction();
 
         if (fragmentA != null) {
             transaction.detach(fragmentA);
+            transaction.addToBackStack("DetachFragA");
             transaction.commit();
         } else {
             Toast.makeText(this, "Fragment A not Found", Toast.LENGTH_SHORT).show();
@@ -106,12 +108,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showFragmentA(View view) {
-
         FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
         FragmentTransaction transaction = manager.beginTransaction();
 
         if (fragmentA != null) {
             transaction.show(fragmentA);
+            transaction.addToBackStack("ShowFragA");
             transaction.commit();
         } else {
             Toast.makeText(this, "Fragment A not Found", Toast.LENGTH_SHORT).show();
@@ -119,15 +121,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void hideFragmentA(View view) {
-
         FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
         FragmentTransaction transaction = manager.beginTransaction();
 
         if (fragmentA != null) {
             transaction.hide(fragmentA);
+            transaction.addToBackStack("HideFragA");
             transaction.commit();
         } else {
             Toast.makeText(this, "Fragment A not Found", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void dummyBackButtonClick(View view) {
+        manager.popBackStack();
+    }
+
+    public void pop_AddFragA_Inclusive_Transaction(View view) {
+        manager.popBackStack("AddFragA", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+    }
+
+    public void pop_AddFragB_Transaction(View view) {
+        manager.popBackStack("AddFragB", 0);
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        int length = manager.getBackStackEntryCount();
+
+        StringBuilder msg = new StringBuilder("");
+
+        for (int i = length - 1; i >= 0; i--) {
+            msg.append("Index ").append(i).append(" : ");
+            msg.append(manager.getBackStackEntryAt(i).getName());
+            msg.append(" \n");
+        }
+
+        Log.i(TAG, "\n" + msg.toString() + " \n ");
     }
 }
